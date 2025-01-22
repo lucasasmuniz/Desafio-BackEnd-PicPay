121b9b03bb85e02767469939fd7cbf53239322a5
Lucas Muniz <lucasmunizfk@gmail.com>
2025-01-21 23:37:43 -0300

    feat: Transaction and user Controller

--- src/main/java/br/com/desafiopicpay/desafiopicpay/service/UserService.java
+++ src/main/java/br/com/desafiopicpay/desafiopicpay/service/UserService.java
@@ -1,5 +1,7 @@
 package br.com.desafiopicpay.desafiopicpay.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,5 +20,9 @@ public class UserService {
 
     public User findUserById(Long id){
         return userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found!"));
+    }
+
+    public List<User> findAllUsers(){
+        return userRepository.findAll();
     }
 }
