560d38768e9fa2c9dfdd5d0570650c17c4342c5e
Lucas Muniz <lucasmunizfk@gmail.com>
2025-01-21 22:45:16 -0300

    Created services

--- src/main/java/br/com/desafiopicpay/desafiopicpay/service/UserService.java
+++ src/main/java/br/com/desafiopicpay/desafiopicpay/service/UserService.java
@@ -1,12 +1,9 @@
 package br.com.desafiopicpay.desafiopicpay.service;
 
-import java.math.BigDecimal;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.desafiopicpay.desafiopicpay.model.User;
-import br.com.desafiopicpay.desafiopicpay.model.UserType;
 import br.com.desafiopicpay.desafiopicpay.repository.UserRepository;
 
 @Service
@@ -15,21 +12,11 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    public void createUser(User user){
+    public void saveUser(User user){
         this.userRepository.save(user);
     }
 
     public User findUserById(Long id){
         return userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found!"));
-    }
-
-    public void validateTransaction(User payee, BigDecimal amount){
-        if (payee.getUserType() == UserType.COMMON) {
-            throw new RuntimeException("Retailers cannot make transfers");
-        };
-
-        if (payee.getBalance().compareTo(amount) == 0){
-            throw new RuntimeException("User does not have sufficient balance to complete the transaction");
-        }
     }
 }
